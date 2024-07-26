a54b8fdff1aedb64aa435e9ff4f0ab05dcb5b3f6
ghufron akbar <alan.040219.gam@gmail.com>
2024-07-26 21:32:07 +0700

    Event Ease

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+/** @type {import('tailwindcss').Config} */
 
 const config: Config = {
   content: [
@@ -12,20 +13,24 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-          "abstract-1": "url('/wallpaper/wallpaper1.jpg')",
+        "abstract-1": "url('/wallpaper/wallpaper1.jpg')",
+        "event-ease": "url('/event-ease/wallpaper.webp')",
       },
       colors: {
-        primary:"#1D171E",
-        secondary:"#1B151C",
-        tertiary:"#1C151D",
-        accent:"#A57399",
+        primary: "#1D171E",
+        secondary: "#1B151C",
+        tertiary: "#1C151D",
+        accent: "#A57399",
       },
       fontFamily: {
         bebas: ["Bebas Neue", "sans-serif"],
         montserrat: ["Montserrat", "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        ubuntu: ["Ubuntu", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/aspect-ratio")],
 };
 export default config;
